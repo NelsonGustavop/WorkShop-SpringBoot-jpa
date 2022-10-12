7f8ebaf3d93778d72a93a83ccc4e593b0abb5659
THE ONE ABOVE ALL <nelsonred100@gmail.com>
2022-10-11 21:20:41 -0300

    Category create

--- src/main/java/com/Slayer/spring/config/TestConfig.java
+++ src/main/java/com/Slayer/spring/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.Slayer.spring.entities.Category;
 import com.Slayer.spring.entities.Order;
 import com.Slayer.spring.entities.User;
 import com.Slayer.spring.entities.enuns.OrderStatus;
+import com.Slayer.spring.repositories.CategoryRepository;
 import com.Slayer.spring.repositories.OrderRepository;
 import com.Slayer.spring.repositories.UserRepository;
 
@@ -20,21 +22,35 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	
 	@Autowired
 	private OrderRepository orderRepository;
-
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 		User u1 = new User(null, "Eren", "ErenYeger@gmail.com", "123");
 		User u2 = new User(null, "Erwin", "Erwin@gmail.com", "123");
 
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMMENT, u1);
-
+		
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
 	}
 
 }
